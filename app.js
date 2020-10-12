5272c2c8702e9e2e5ebedcc91f1936731ad5d914
saleemBarakat <saleembarakat00@gmail.com>
2020-10-12 15:50:56 +0200

    added buttons

--- app.js
+++ app.js
@@ -61,3 +61,69 @@ function mySearchFunction (){
           }
         }
       }
+
+let courses = [
+    { id: 1, title: "Administratör" },
+    { id: 2, title: "Målare" },
+    { id: 3, title: "Undersköterska LSS" },
+    { id: 4, title: "Golvläggare" },
+    { id: 5, title: "Bygg-Träarbete" },
+    { id: 6, title: "VVS montör" },
+    { id: 7, title: "Nätverkstekniker" },
+    { id: 8, title: "Automationselektriker" },
+    { id: 9, title: "Vård och omsorgsutbildning" },
+    { id: 10, title: "Murare" },
+    { id: 11, title: "Bygg-Anläggning" },
+  ];
+  
+  courses.forEach((course) => {
+    createCourse(course.id, course.title);
+  });
+  
+  function createCourse(id, title) {
+    var li = document.createElement("div");
+    li.id = id;
+      var button = '<button onClick="checkId(this)" class="knapp">Lägg till</button>';
+
+    var h4 = document.createElement("div");
+    h4.className = "bla";
+    h4.id = id;
+    h4.innerHTML = title;
+  
+  
+    li.appendChild(h4);
+    li.insertAdjacentHTML("beforeend", button);
+  
+    document.getElementById("courseList").appendChild(li);
+  }
+  
+  function checkId(clickedElement) {
+    addSelectedCourse(clickedElement.parentNode.id);
+  }
+  
+  function addSelectedCourse(id) {
+
+    var selectedCourseAlreadySelected = document.getElementById(courses[id - 1].title);
+  
+    if (!selectedCourseAlreadySelected) {
+      var li = document.createElement("LI");
+  
+      li.id = id;
+      li.setAttribute("id", courses[id - 1].title);
+  
+      var h4 = document.createElement("h4");
+      h4.innerHTML = courses[id - 1].title;
+  
+      li.appendChild(h4);
+  
+      var deleteButton = '<button onClick="removeCourse(this)">Ta bort</button>';
+  
+      li.insertAdjacentHTML("beforeend", deleteButton);
+  
+      document.getElementById("selectedCourses").appendChild(li);
+    }
+  }
+  
+  function removeCourse(course) {
+    document.getElementById(course.parentNode.id).remove();
+  }
