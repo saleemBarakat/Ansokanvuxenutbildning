491fffe6950d9c7ada5da356f351e3cb8d154639
saleemBarakat <saleembarakat00@gmail.com>
2020-10-19 00:48:21 +0200

    updated and made the last updates on the web page for now

--- app.js
+++ app.js
@@ -24,3 +24,18 @@ function myFunction() {
   function displayDate() {
     document.getElementById("datum").innerHTML = Date();
   }
+  
+//logga in*/
+  $(document).ready(function(){
+    $("#submit").click(function(){
+      var name= $("#name").val();
+      var pass = $("#pass").val();
+      if(name == '' || pass == ''){
+        $("#Required").html('All Feild Are Required').css('color','red');
+      }else if(name == 'admin' && pass == '123'){
+         $("#form").html('<h4>User Login Successfully</h4><a href="">Back</a>').css('color','green');
+       }else{
+         $("#error").html('User Are Not Valid');
+       }
+    });
+  });
